f0bcd2a5219ed24da5f56ef7e2c1f9440cf689fa
Alex Wardi <aaw5017@gmail.com>
2023-11-30 23:14:24 -0500

    Adding migrations folder

---
+++ migrations/20231111130418_create.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY NOT NULL,
+  email TEXT NOT NULL UNIQUE,
+  handle TEXT NOT NULL,
+  hashed_password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+  id TEXT PRIMARY KEY NOT NULL,
+  csrf_token TEXT NOT NULL,
+  user_id INTEGER NOT NULL,
+  created_at INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
